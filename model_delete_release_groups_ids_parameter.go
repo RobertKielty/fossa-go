ec45e3a8b6ca607f78833c761e0904ea7d03361d
RobertKielty <208059+RobertKielty@users.noreply.github.com>
2026-07-20 09:17:51 +0000

    chore: update swagger spec

--- model_delete_release_groups_ids_parameter.go
+++ model_delete_release_groups_ids_parameter.go
@@ -3,7 +3,7 @@ FOSSA API
 
 OpenAPI Specification for public FOSSA APIs
 
-API version: 4.31.29
+API version: 4.34.42
 Contact: support@fossa.com
 */
 
@@ -85,7 +85,16 @@ func (dst *DeleteReleaseGroupsIdsParameter) UnmarshalJSON(data []byte) error {
 	} else if match == 1 {
 		return nil // exactly one match
 	} else { // no match
-		return fmt.Errorf("data failed to match schemas in oneOf(DeleteReleaseGroupsIdsParameter)")
+        if err != nil {
+            return fmt.Errorf("data failed to match schemas in oneOf(DeleteReleaseGroupsIdsParameter): %v", err)
+        } else {
+            return fmt.Errorf("data failed to match schemas in oneOf(DeleteReleaseGroupsIdsParameter)")
+        }
+        if err != nil {
+            return fmt.Errorf("data failed to match schemas in oneOf(DeleteReleaseGroupsIdsParameter): %v", err)
+        } else {
+            return fmt.Errorf("data failed to match schemas in oneOf(DeleteReleaseGroupsIdsParameter)")
+        }
 	}
 }
 
