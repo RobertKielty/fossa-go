38e1143f2be6f3d0ee0abebd5add4eccc4c30a1c
RobertKielty <208059+RobertKielty@users.noreply.github.com>
2026-07-25 08:08:44 +0000

    chore: update swagger spec

--- model_update_issues_request_one_of_1_ticket.go
+++ model_update_issues_request_one_of_1_ticket.go
@@ -3,7 +3,7 @@ FOSSA API
 
 OpenAPI Specification for public FOSSA APIs
 
-API version: 4.31.29
+API version: 4.34.47
 Contact: support@fossa.com
 */
 
@@ -85,7 +85,16 @@ func (dst *UpdateIssuesRequestOneOf1Ticket) UnmarshalJSON(data []byte) error {
 	} else if match == 1 {
 		return nil // exactly one match
 	} else { // no match
-		return fmt.Errorf("data failed to match schemas in oneOf(UpdateIssuesRequestOneOf1Ticket)")
+        if err != nil {
+            return fmt.Errorf("data failed to match schemas in oneOf(UpdateIssuesRequestOneOf1Ticket): %v", err)
+        } else {
+            return fmt.Errorf("data failed to match schemas in oneOf(UpdateIssuesRequestOneOf1Ticket)")
+        }
+        if err != nil {
+            return fmt.Errorf("data failed to match schemas in oneOf(UpdateIssuesRequestOneOf1Ticket): %v", err)
+        } else {
+            return fmt.Errorf("data failed to match schemas in oneOf(UpdateIssuesRequestOneOf1Ticket)")
+        }
 	}
 }
 
