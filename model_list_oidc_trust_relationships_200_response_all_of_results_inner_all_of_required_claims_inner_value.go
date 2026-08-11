fe9fdb0febc58834910e991eda6deb7443c33c78
RobertKielty <208059+RobertKielty@users.noreply.github.com>
2026-08-11 07:10:35 +0000

    chore: update swagger spec

--- model_list_oidc_trust_relationships_200_response_all_of_results_inner_all_of_required_claims_inner_value.go
+++ model_list_oidc_trust_relationships_200_response_all_of_results_inner_all_of_required_claims_inner_value.go
@@ -3,7 +3,7 @@ FOSSA API
 
 OpenAPI Specification for public FOSSA APIs
 
-API version: 4.31.29
+API version: 4.34.64
 Contact: support@fossa.com
 */
 
@@ -111,7 +111,21 @@ func (dst *ListOIDCTrustRelationships200ResponseAllOfResultsInnerAllOfRequiredCl
 	} else if match == 1 {
 		return nil // exactly one match
 	} else { // no match
-		return fmt.Errorf("data failed to match schemas in oneOf(ListOIDCTrustRelationships200ResponseAllOfResultsInnerAllOfRequiredClaimsInnerValue)")
+        if err != nil {
+            return fmt.Errorf("data failed to match schemas in oneOf(ListOIDCTrustRelationships200ResponseAllOfResultsInnerAllOfRequiredClaimsInnerValue): %v", err)
+        } else {
+            return fmt.Errorf("data failed to match schemas in oneOf(ListOIDCTrustRelationships200ResponseAllOfResultsInnerAllOfRequiredClaimsInnerValue)")
+        }
+        if err != nil {
+            return fmt.Errorf("data failed to match schemas in oneOf(ListOIDCTrustRelationships200ResponseAllOfResultsInnerAllOfRequiredClaimsInnerValue): %v", err)
+        } else {
+            return fmt.Errorf("data failed to match schemas in oneOf(ListOIDCTrustRelationships200ResponseAllOfResultsInnerAllOfRequiredClaimsInnerValue)")
+        }
+        if err != nil {
+            return fmt.Errorf("data failed to match schemas in oneOf(ListOIDCTrustRelationships200ResponseAllOfResultsInnerAllOfRequiredClaimsInnerValue): %v", err)
+        } else {
+            return fmt.Errorf("data failed to match schemas in oneOf(ListOIDCTrustRelationships200ResponseAllOfResultsInnerAllOfRequiredClaimsInnerValue)")
+        }
 	}
 }
 
